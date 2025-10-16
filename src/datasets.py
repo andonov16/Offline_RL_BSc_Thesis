8bec8f075957eb1ea4c341cfaba51e88145da44a
andonov16 <miro.and345@gmail.com>
2025-10-16 13:00:45 +0200

    BCDataset class expects tensors instead of np.array

--- src/datasets.py
+++ src/datasets.py
@@ -1,16 +1,20 @@
-import numpy as np
 import torch
 from torch.utils.data import Dataset
 
 
 # Dataset class used for training the Behaviour Cloning (BC)
-class BCDataset(Dataset):
-    def __init__(self, observations: np.array, actions: np.array):
-        self.observations, self.actions = torch.tensor(observations), torch.tensor(actions)
-        self.__size__ = len(self.actions)
+class BC_Dataset(Dataset):
+    def __init__(self, states: torch.Tensor,
+                 actions: torch.Tensor):
+        self.states, self.actions = states, actions
+        if not isinstance(states, torch.Tensor):
+            self.states = torch.tensor(self.states)
+        if not isinstance(actions, torch.Tensor):
+            self.actions = torch.tensor(self.actions)
+        self.__size__ = self.actions.size()
 
     def __len__(self):
         return self.__size__
 
     def __getitem__(self, index):
-        return self.observations[index], self.actions[index]
+        return self.states[index], self.actions[index]
